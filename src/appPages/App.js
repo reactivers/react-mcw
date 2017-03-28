a26e4a245edc1f2423ecdee777d624c62f54fdc1
mrtgny <muratguney1995@gmail.com>
2017-03-29 00:52:57 +0300

    Tabs Componenti Eklendi

--- src/appPages/App.js
+++ src/appPages/App.js
@@ -1,49 +1,31 @@
 import React, {Component} from 'react';
 import './App.css';
 import FABMenu from './FabMenu'
-import Drawer from './Drawer'
-import {RadioButton} from './RadioButton'
-import {List, ListItem} from './List'
-import "@material/list/dist/mdc.list.css";
-
+import {Tabs, Tab} from './Tab'
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {open: false,radio:false}
+        this.state = {
+            open: false,
+            radio: false,
+            tabIndex: 1,
+        }
 
     }
-
-    radioBtnHandler(){
-        this.setState({radio:!this.state.radio},()=>console.log(this.state.radio))
-    }
-
 
     render() {
         return (
-            <div>
-                <Drawer open={this.state.open} headerStyle={{padding:0}} header={<span style={{
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "green"
-                }}>Title</span>}
-                        onClose={() => this.setState({open: !this.state.open})}>
-                    <div>
-                    <RadioButton label="Clicked" setChecked={this.state.radio} isChecked={e=>console.log("isChecked",e)} onChange={e=>console.log("onchange",e)}/>
-                    <button id="btn" onClick={()=>console.log("aaa")}>radioChanger</button>
-                    <List>
-                        <ListItem dividerInset>Başlık</ListItem>
-                        <ListItem dividerInset>İçerik</ListItem>
-                        <ListItem dividerInset>Konu</ListItem>
-                    </List>
-                    </div>
-                </Drawer>
-                <button onClick={()=>console.log("aaa")}>radioChanger</button>
-                <button onClick={() => this.setState({open: !this.state.open})}>open</button>
+            <div className="rmd-centered-item" style={{width: "100%", height: "100%"}}>
+                <div>
+                    <Tabs>
+                        <Tab label="Tab 3">
+                        </Tab>
+                        <Tab label="Tab 3">
+                        </Tab>
+                    </Tabs>
+                </div>
                 <FABMenu showAdd={1} showCopy={2} showDelete={3} showPeopleAdd={4} showSearch={5}/>
             </div>
         );
