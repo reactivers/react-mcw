d8928e79c0070a892936cd74a6b67d3f68dedac2
mrtgny <muratguney1995@gmail.com>
2017-04-01 01:36:06 +0300

    Badge,Chip and Acordion components added

---
+++ src/Documentation/ChipPage.js
@@ -0,0 +1,112 @@
+/**
+ * Created by muratguney on 29/03/2017.
+ */
+import React from 'react'
+import {Card, CardHeader, CardActions, CardBody} from '../Card';
+import Chip from '../Chip';
+import Acordion from '../Acordion';
+import Button from '../Button';
+import {List, ListItem} from '../List';
+
+
+const data2 = [
+    {dsc: "List 7",id:1},
+    {dsc: "List 8",id:1},
+    {dsc: "List 9",id:1},
+    {dsc: "List 10",id:1},
+    {dsc: "List 11",id:1},
+    {dsc: "List 12",id:1},
+];
+const data3 = [
+    {dsc: "List 13",id:2},
+    {dsc: "List 14",id:2},
+    {dsc: "List 15",id:2},
+    {dsc: "List 16",id:3},
+    {dsc: "List 17",id:3},
+    {dsc: "List 18",id:3},
+];
+const data4 = [
+    {dsc: "List 19"},
+    {dsc: "List 20"},
+    {dsc: "List 21"},
+    {dsc: "List 22"},
+    {dsc: "List 23"},
+    {dsc: "List 24"},
+];
+
+const data5 = [
+    {dsc: "List 25"},
+    {dsc: "List 26"},
+    {dsc: "List 27"},
+    {dsc: "List 28"},
+    {dsc: "List 29"},
+    {dsc: "List 30"},
+];
+
+
+
+const data1 = [
+    {dsc: "List 1",id:1,children:data2},
+    {dsc: "List 2",id:2,children:data3},
+    {dsc: "List 3",id:3,children:data4},
+    {dsc: "List 4",id:4,children:data5},
+    {dsc: "List 5",id:5},
+    {dsc: "List 6",id:6},
+];
+
+
+
+export default class ChipPage extends React.Component {
+    state = {};
+
+    render() {
+        return (
+            <Card style={{justifyContent: "flex-start",height:"100%"}}>
+                <CardHeader title="Title"/>
+
+                <Chip avatar={<div>Selam</div>}
+                      avatarStyle={{width:"auto",padding:4,marginLeft:-4,fontSize:14}}
+                      onCancel={()=>{}}
+                      style={{backgroundColor:"lightblue"}}>
+                    Selam Ben Murat Gardaş
+                </Chip>
+
+
+
+                <Acordion label="İlk">
+                    <Acordion label="İki" />
+                    <Acordion label="Üç" />
+                    <Acordion label="Dört" />
+                </Acordion>
+                <Acordion label="Ana">
+                    <Acordion label="İki" />
+                    <Acordion label="Üç" />
+                    <Acordion label="Dört" />
+                </Acordion>
+
+
+                <Acordion itemHeight={48} data={data1} child={(child,index)=> {
+                    if(child.item.children)
+                        return(
+                            <Acordion data={child.item.children} child={(child2,index2)=>{
+                                return(
+                                    <Card style={{justifyContent: "flex-start",height:"100%"}}>
+                                        <CardHeader title="Title"/>
+                                        <CardBody>{child2.item.dsc}</CardBody>
+                                    </Card>
+                                )
+                            }} />
+                        );
+                    else {
+                        return(
+                            <span>aaa</span>
+                        )
+                    }
+                }
+                }>
+                </Acordion>
+
+            </Card>
+        )
+    }
+}
---
+++ src/DragAndDrop/index.js
@@ -0,0 +1,80 @@
+import React from 'react'
+class DragAndDrop extends React.Component{
+
+    state={};
+
+
+    componentDidMount() {
+        this.oldMouseDown = document.onmousedown;
+        this.oldMouseUp = document.onmouseup;
+
+        document.onmouseup = function (e) {
+            this.setState({dad: false});
+            this.oldMouseUp(e)
+        }.bind(this);
+
+        document.onmousedown = function (e) {
+            if (e.target.classList.value.indexOf("iota-draggable") > -1) {
+                let dad = e.target;
+                let position = dad.getBoundingClientRect();
+                let dadOffsetX = parseInt(dad.style.left);
+                let dadOffsetY = parseInt(dad.style.top);
+                if(!dadOffsetX)
+                    dadOffsetX = 0;
+                if(!dadOffsetY)
+                    dadOffsetY = 0;
+
+                if(!this.state.initial){
+                    this.setState({initial:{x:position.left - dadOffsetX,y:position.top - dadOffsetY,parentHeight:e.srcElement.offsetParent.clientHeight,parentWidth:e.srcElement.offsetParent.clientWidth}})
+                }
+                this.setState({dad: true,offset:{x:(e.clientX - position.left),y:(e.clientY - position.top)}});
+            }
+            this.oldMouseDown(e)
+        }.bind(this);
+
+        document.onmousemove = function (e) {
+            let dad = e.target;
+            let position = dad.getBoundingClientRect();
+
+
+            let dadOffsetX = parseInt(dad.style.left);
+            let dadOffsetY = parseInt(dad.style.top);
+            if(!dadOffsetX)
+                dadOffsetX = 0;
+            if(!dadOffsetY)
+                dadOffsetY = 0;
+
+
+            if (this.state.dad && e.target.classList.value.indexOf("iota-draggable") > -1) {
+
+                const isRightEdgeOut = (dadOffsetX + position.width) <= this.state.initial.parentWidth;
+                const isMouseDragToLeft =  e.clientX < (position.left + this.state.offset.x);
+
+                const isBottomEdgeOut = (dadOffsetY + position.height) <= this.state.initial.parentHeight;
+                const isMouseDragToTop =  e.clientY < (position.top + this.state.offset.y);
+
+                const isTopEdgeOut = (e.clientY - position.top) + dadOffsetY - this.state.offset.y >= 0;
+                const isMouseDragToBottom = e.clientY > (position.top + this.state.offset.y);
+
+                const isLeftEdgeOut = (e.clientX - position.left) + dadOffsetX - this.state.offset.x >=0;
+                const isMouseDragToRight = e.clientX > (position.left + this.state.offset.x);
+
+                if(!this.props.limitToEdge || (isRightEdgeOut ||isMouseDragToLeft) && (isBottomEdgeOut || isMouseDragToTop ) && (isTopEdgeOut || isMouseDragToBottom) && (isLeftEdgeOut || isMouseDragToRight) ){
+
+                    dad.style.left = ((e.clientX - position.left) + dadOffsetX - this.state.offset.x + 'px');
+                    dad.style.top = ((e.clientY - position.top) + dadOffsetY - this.state.offset.y  + 'px');
+
+                }
+
+            }
+
+        }.bind(this)
+    }
+
+
+    render(){
+
+        return null
+    }
+
+}
